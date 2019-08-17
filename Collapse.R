f020ab0ff324e70a6c9bbee651dfbaf64668b593
Ljeevan9211 <Ljeevan9211@gmail.com>
2019-08-17 23:51:51 +0100

    Update Collapse.R
    Added the name of Species and corrected a mistake in filename. Thanks to Simon!

--- Collapse.R
+++ Collapse.R
@@ -1,11 +1,13 @@
 ## reading Detection history
 DH <- read.csv("originaldh.csv")
+Species <- "CloudedLeopard" # Name of your species 
 
 # number of cameratrap days
 IN_Occ <- ncol(DH) -1
 
 ## defining the occassion length
-OL <- 5 # one occasion is 5 days 
+# OL <- 5 # one occasion is 5 days 
+OL <- c(3,5,7,13) ## if you want different length of occassions
 
 ## getting the new Occasion length
 Occ_no <- IN_Occ%/%OL # getting the collapsed occasion length
@@ -13,19 +15,20 @@ Occ_no
 N <- nrow(DH)
 head(DH)
 
+## Now since we got different length of occassions we run a loop
+for(k in 1:length(Occ_no)){
 ## create a new matrix to keep the new DH
-DH2 <-  matrix(nrow = N, ncol = Occ_no)
-
+DH2 <-  matrix(nrow = N, ncol = Occ_no[k])
 # now doing the loop to get the maximum value from within occasion days
-for(i in 1:N){
-for(j in 1:Occ_no){
-DH2[i, j] <- max(DH[i,(((j-1)*OL)+2):((OL*j)+1)]) ## 2 here because the first column is trapsite
-}}
-
-## Making it neat
-colnames(DH2) <- paste("Occ", as.character(1:Occ_no))
-rownames(DH2) <- DH[,1]
-DH2
-
-# creating a csv file
-write.csv(DH2, "DH.csv")
+	for(i in 1:N){
+		for(j in 1:Occ_no[k]){
+			DH2[i, j] <- max(DH[i,(((j-1)*OL[k])+2):((OL[k]*j)+1)]) ## 2 here because the first column is trapsite
+			}}
+		## Making it neat
+		colnames(DH2) <- paste("Occ", as.character(1:Occ_no[k]))
+		rownames(DH2) <- DH[,1]
+	# creating a csv file
+  name <- paste(Species, "_Occ", as.character(OL[k]),".csv")
+  name <- gsub(" ", "", name) 
+	write.csv(DH2, name)
+}
